6fb8a8500366552c03d10051aa73f8be84bf80e9
ZMA <sultan040419@gmail.com>
2022-08-27 14:41:36 +0300

    Задача 15

---
+++ Задача15/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите целое число от 1 до 7: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a == 1)
+{
+    Console.WriteLine("нет");
+}
+else if(a == 2)
+{
+    Console.WriteLine("нет");
+}
+else if(a == 3)
+{
+    Console.WriteLine("нет");
+}
+else if(a == 4)
+{
+    Console.WriteLine("нет");
+}
+else if(a == 5)
+{
+    Console.WriteLine("нет");
+}
+else if( a==6 )
+{
+    Console.WriteLine("да");
+}
+else if( a==7 )
+{
+    Console.WriteLine("да");
+}
